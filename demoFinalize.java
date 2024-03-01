e43de27c0ab737bb8ddaa0eaa304ae734818e83b
SurabiNagarajWPTB <161475212+SurabiNagarajWPTB@users.noreply.github.com>
2024-03-01 20:31:54 +0530

    Learn and Understand use of finalize( ) method in java for explicit garbage Collection and implement the same.

--- demoFinalize.java
+++ demoFinalize.java
@@ -1,39 +1,39 @@
-package Week03;
+package Week04;
 
 import java.util.Scanner;
 public class demoFinalize {
 	public static void main(String[] args) {
-		student1 kabir=new student1();
-		kabir.display();
-		System.out.println("object reference is ..."+kabir);
-		kabir.finalize();
+		student1 harsha = new student1();
+		harsha.display();
+		System.out.println("Object reference is..."+harsha);
+		harsha.finalize();
 		System.gc();
-		System.out.println("object garbage collected");
+		System.out.println("Object garbage collected");
 	}
 }
 class student1
 {
 	String name;
-    int rno;
-    float result;
-    Scanner sc= new Scanner(System.in);
-    student1()
+	int rno;
+	float res;
+	Scanner sc = new Scanner(System.in);
+	student1()
     {
-    	System.out.println("enter roll no of student");
-        rno=Integer.parseInt(sc.next());
-        System.out.println("enter the name of the student");
-        name=sc.next();
-        System.out.println("enter the result of the student");
-        result=Float.parseFloat(sc.next());
+		System.out.println("Enter Roll Number of Student:");
+		rno=Integer.parseInt(sc.next());
+		System.out.println("Enter Name of Student:");
+		name=sc.next();
+		System.out.println("Enter result of the Student:");
+		res = Float.parseFloat(sc.next());
     }
-    void display()
-    {
-    	System.out.println(rno+"\t"+name+"\t"+result);
-    }
-    @Override
-    protected void finalize()
-    {
-    	sc.close();
-    	System.out.println("object cleaned up by finalize method");
-    }
+	void display()
+	{
+		System.out.println(rno+"\t\t"+name+"\t\t"+res);
+	}
+	@Override
+	protected void finalize()
+	{
+		sc.close();
+		System.out.println("Object Cleaned Up by Finalize Method.");
+	}
 }
