b9f555b71ba09f8aa75d39b8666939b464ed0fd0
SurabiNagarajWPTB <161475212+SurabiNagarajWPTB@users.noreply.github.com>
2024-03-14 21:59:17 +0530

    Check positive or negative number?

---
+++ PostiveOrNegative.java
@@ -0,0 +1,15 @@
+package Week02;
+
+public class PostiveOrNegative {
+
+	public static void main(String[] args) {
+		int num=2;
+		if (num>0) {
+			System.out.println("The number is postive");
+		}
+		else {
+			System.out.println("The number is negative");
+		}
+	}
+
+}
