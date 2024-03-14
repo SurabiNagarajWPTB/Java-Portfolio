a17bfea2a3c65a0898058c8473db9079746f29d8
SurabiNagarajWPTB <161475212+SurabiNagarajWPTB@users.noreply.github.com>
2024-03-14 22:56:23 +0530

    WAP to read a character and check if its Vowel or not?

---
+++ VowelOrNot.java
@@ -0,0 +1,16 @@
+package Week02;
+
+public class VowelOrNot {
+
+	public static void main(String[] args) {
+		System.out.println("This program checks whether the given character is vowel or not");
+		char ch='A';
+		if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+			System.out.println("The given character is vowel");
+		else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			System.out.println("The given character is vowel");
+		else
+			System.out.println("The given character is not vowel");
+	}
+
+}
